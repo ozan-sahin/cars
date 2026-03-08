173a2cf175645c2a16f64cbf70e35c98b6e5e1b5
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2026-03-08 13:08:46 +0100

    Update Welcome_cars.py

--- streamlit/Welcome_cars.py
+++ streamlit/Welcome_cars.py
@@ -251,7 +251,7 @@ df_costs_buying = pd.DataFrame({
     "Insurance": insurance / 12,
     "Tax": tax / 12,
     "Fuel": fuel / 12,
-    "Maintenance": maintenance / 12 * 1.1  # Buying maintenance is 10% higher
+    "Maintenance": maintenance / 12 * 1.2  # Buying maintenance is 20% higher
 })
 
 
@@ -336,5 +336,6 @@ st.dataframe(df[mask].reset_index(drop=True), use_container_width=True, hide_ind
              column_config={"image": st.column_config.ImageColumn("Image"), "price": st.column_config.NumberColumn("Price (€)", format="€ %.0f"),
                             "distance": st.column_config.NumberColumn("Distance (km)", format="%.0f km"),
                             "age": st.column_config.NumberColumn("Age (years)", format="%.0f"), "url": st.column_config.LinkColumn("Link", width="small")})
+
 
 
