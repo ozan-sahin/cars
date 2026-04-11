cb859adb819f13f237a5c1bdef9ee3bdbdb5e787
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2026-04-11 15:51:51 +0200

    Update Welcome_cars.py

--- streamlit/Welcome_cars.py
+++ streamlit/Welcome_cars.py
@@ -314,22 +314,22 @@ with c1:
 
     fig = go.Figure()
     for col in ["Leasing", "Fuel","Insurance","Tax","Maintenance"]:
-        fig.add_bar(x=df_costs_leasing[period], y=df_costs_leasing[col], name=col)
+        fig.add_bar(x=df_costs_leasing[period], y=df_costs_leasing[col], name=col,  marker=dict(color=color_map[col]))
     fig.add_scatter(x=df_costs_leasing[df_costs_leasing.columns[0]], y=df_costs_leasing["Total"], mode="lines+markers",
                     name="Total", line=dict(color="orange",width=2), yaxis="y2")
     fig.update_layout(barmode="stack", title=f"{period}ly Cost Breakdown of {brand_2}-{model_2} for Leasing at {leasing_price} €/month", xaxis_title=period, legend=dict(orientation="h", y=1.15, x=0),
-                      yaxis_title="Cost Components (€)", yaxis2=dict(title="Total Cost (€)", overlaying="y", side="right", autorange=True, showgrid=False),  marker=dict(color=color_map[col]), width=800, height=500)
+                      yaxis_title="Cost Components (€)", yaxis2=dict(title="Total Cost (€)", overlaying="y", side="right", autorange=True, showgrid=False), width=800, height=500)
     st.plotly_chart(fig,use_container_width=True)
 
 
 with c2:
     fig = go.Figure()
     for col in [ "Fuel","Insurance","Tax","Maintenance", "Down_Payment", "Zinsen", "Tilgung"]:
-        fig.add_bar(x=df_costs_buying[period], y=df_costs_buying[col], name=col)
+        fig.add_bar(x=df_costs_buying[period], y=df_costs_buying[col], name=col, marker=dict(color=color_map[col]))
     fig.add_scatter(x=df_costs_buying[df_costs_buying.columns[0]], y=df_costs_buying["Total"], mode="lines+markers",
                     name="Total", line=dict(color="orange",width=2), yaxis="y2")
     fig.update_layout(barmode="stack", title=f"{period}ly Cost Breakdown of {brand_2}-{model_2} for Buying at {initial_buy} € initial buy", xaxis_title=period, legend=dict(orientation="h", y=1.15, x=0),
-                      yaxis_title="Cost Components (€)", yaxis2=dict(title="Total Cost (€)", overlaying="y", side="right", autorange=True, showgrid=False),  marker=dict(color=color_map[col]), width=800, height=500)
+                      yaxis_title="Cost Components (€)", yaxis2=dict(title="Total Cost (€)", overlaying="y", side="right", autorange=True, showgrid=False), width=800, height=500)
     st.plotly_chart(fig,use_container_width=True)
 
 # st.dataframe(df_costs_leasing, use_container_width=True)
