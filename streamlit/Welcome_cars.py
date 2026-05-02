ad2d36c5006dbd3c0e70ddf75ab9b1a9a510e87c
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2026-05-02 14:29:55 +0200

    Update Welcome_cars.py

--- streamlit/Welcome_cars.py
+++ streamlit/Welcome_cars.py
@@ -371,8 +371,8 @@ with column44:
 mask = df.query("price >= @low_price and price <= @high_price") \
         .query("query_date.dt.strftime('%Y-%m-%d') in @dates")
 
-if HAS_POWER:
-    mask &= df.transmission_kw <= engine_power
+# if HAS_POWER:
+#     mask &= df.transmission_kw <= engine_power
 
 st.dataframe(df.reset_index(drop=True), use_container_width=True, hide_index=True,
              column_config={"image": st.column_config.ImageColumn("Image"), "price": st.column_config.NumberColumn("Price (€)", format="€ %.0f"),
