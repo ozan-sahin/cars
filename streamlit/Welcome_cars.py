27e7dd101c33923ec638f0bc7426669240e6a6a2
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2026-05-02 14:09:44 +0200

    Update Welcome_cars.py

--- streamlit/Welcome_cars.py
+++ streamlit/Welcome_cars.py
@@ -372,8 +372,8 @@ mask = df.query("price >= @low_price and price <= @high_price") \
 if HAS_POWER:
     mask &= df.transmission_kw <= engine_power
 
-st.dataframe(df[mask].reset_index(drop=True), use_container_width=True, hide_index=True,
-             column_config={"image": st.column_config.ImageColumn("Image"), "price": st.column_config.NumberColumn("Price (€)", format="€ %.0f"),
-                            "distance": st.column_config.NumberColumn("Distance (km)", format="%.0f km"),
-                            "age": st.column_config.NumberColumn("Age (years)", format="%.0f"), "url": st.column_config.LinkColumn("Link", width="small")})
+# st.dataframe(df[mask].reset_index(drop=True), use_container_width=True, hide_index=True,
+#              column_config={"image": st.column_config.ImageColumn("Image"), "price": st.column_config.NumberColumn("Price (€)", format="€ %.0f"),
+#                             "distance": st.column_config.NumberColumn("Distance (km)", format="%.0f km"),
+#                             "age": st.column_config.NumberColumn("Age (years)", format="%.0f"), "url": st.column_config.LinkColumn("Link", width="small")})
 
