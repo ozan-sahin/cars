a026e5c8e8b6a8cd64da8b861d96907359177ca8
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2026-05-02 15:32:06 +0200

    Update Welcome_cars.py

--- streamlit/Welcome_cars.py
+++ streamlit/Welcome_cars.py
@@ -91,13 +91,13 @@ if option == "Autoscout24":
     df = conn.read()
     df['query_date'] = pd.to_datetime(df['query_date'])
     df['url'] = 'https://www.autoscout24.de' + df['url']
-    de_months = {
-        "Mrz": "Mar", "Mai": "May", "Okt": "Oct", "Dez": "Dec"
-    }  # only the ones that differ from English
-    df['first_registration'] = df['first_registration'].astype(str).replace(de_months, regex=True)
-    # Drop rows where Sheets couldn't resolve the value
-    df = df[~df['first_registration'].str.contains(r'#', na=False)]
-    df['first_registration'] = pd.to_datetime(df['first_registration'], format='mixed', dayfirst=False)
+    # de_months = {
+    #     "Mrz": "Mar", "Mai": "May", "Okt": "Oct", "Dez": "Dec"
+    # }  # only the ones that differ from English
+    # df['first_registration'] = df['first_registration'].astype(str).replace(de_months, regex=True)
+    # # Drop rows where Sheets couldn't resolve the value
+    # df = df[~df['first_registration'].str.contains(r'#', na=False)]
+    # df['first_registration'] = pd.to_datetime(df['first_registration'], format='mixed', dayfirst=False)
     # df = load_data_autoscout("dataset2.csv")
 
 elif option == "Kleinanzeigen":
